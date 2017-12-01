bc0f961747f1a54acbe2cae31637f2fe7917d735
Marlon Prudente <m.prudente@btc-banco.com>
2017-12-01 11:48:04 -0200

    Post/GET em Java e Python funcionando

--- Java/auth.java
+++ Java/auth.java
@@ -7,21 +7,18 @@
 package API.Trade;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.security.MessageDigest;
 import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
 import java.util.Base64;
-import java.util.TimeZone;
 import javax.crypto.Mac;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import javax.net.ssl.HttpsURLConnection;
 import static org.apache.commons.codec.binary.Base64.*;
-
 
 /**
  *
@@ -36,96 +33,92 @@ public class Authentication {
         byte[] keyBytes = decodeBase64(secret);
         secretKey = new SecretKeySpec(keyBytes, "HmacSHA256");
         Mac mac = Mac.getInstance("HmacSHA256");
-        
+
         mac.init(secretKey);
 
         byte[] text = data.getBytes();
         return new String(encodeBase64(mac.doFinal(text))).trim();
     }
 
-    public String amx_authorization_header(String APIid, String APIkey, String APIfuncao, String APImetodo, String APIbody) throws Exception {
-        String url1 = "https://broker.negociecoins.com.br/tradeapi/v1/" + APIfuncao;
-        String url = URLEncoder.encode(url1, "UTF-8").toLowerCase();
+    public String amx_authorization_header(String APIurl, String APIid, String APIkey, String APIfuncao, String APImetodo, String APIbody) throws Exception {
+
+        String url = URLEncoder.encode(APIurl, "UTF-8").toLowerCase();
         String metodo = APImetodo.toUpperCase();
-        String content = "";        
+        String requestContentBase64String = "";
         
-        //APIbody = "{\"page\":\"1\",\"pageSize\":\"2\",\"pair\":\"BRLBTC\",\"type\":\"buy\",\"status\":\"pending\"}";
-        System.out.println("APIbody==>" + APIbody);
-        
-        if(APIbody != ""){
-            byte[] post =  APIbody.getBytes("UTF-8"); 
-            MessageDigest md5 = MessageDigest.getInstance("MD5");            
+        /**Calcular contet se o mesmo não for nulo (para requisições tipo POST*/
+        if (APIbody != null && !"".equals(APIbody)) {
+            byte[] post = APIbody.getBytes();
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
             byte[] requestContentHash = md5.digest(post);
-            content = Base64.getUrlEncoder().encodeToString(requestContentHash); 
-            //content = "IpapEctISNd9w+TqkCQUKg==";
-            System.out.println("Content: " + content);
-        }        
-        //Calculando UNIX Time: Gerando um requestTimeStamp
-        Timestamp epochStart = new Timestamp(70, 00, 00, 21, 00, 00, 00);
-        SimpleDateFormat data2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-        data2.setTimeZone(TimeZone.getTimeZone("UTC"));
-        String date3 = data2.format(epochStart.getTime());
-        System.out.println("Data: " + date3);
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-        SimpleDateFormat data1 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-        data1.setTimeZone(TimeZone.getTimeZone("UTC"));
-        String date = data1.format(timestamp.getTime());
-        System.out.println("Data atual: " + date);
-        String time = "" + (( timestamp.getTime()- epochStart.getTime())/1000);
-        System.out.println("TIME: " + time);          
+            requestContentBase64String = Base64.getEncoder().encodeToString(requestContentHash);            
+        }
+
+        /**Calculando UNIX Time: Gerando um requestTimeStamp*/
+        Timestamp epochStart = new Timestamp(70, 00, 00, 21, 00, 00, 00);  /**Parametros para iniciar 01/01/1970 00:00:00 UTC*/
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());       
+        String time = "" + ((timestamp.getTime() - epochStart.getTime()) / 1000); /**Diferença entre datas, é dividido por 1000 para ficar no tamanho correto*/
         
+        /**Gerando NONCE, variável aleatória para autenticação*/
         String nonce = java.util.UUID.randomUUID().toString().replace("-", "");
-        String dados = String.format("%s%s%s%s%s%s", APIid, metodo, url, time, nonce, content);
-        System.out.println("Signature: " + dados);
-        String header = String.format("amx %s:%s:%s:%s", APIid, computeSignature(dados,APIkey), nonce, time); //Formato Header
-        return header;
+        
+        /**Criando assinatura*/
+        String signatureRawData = String.format("%s%s%s%s%s%s", APIid, metodo, url, time, nonce, requestContentBase64String);
+        System.out.println("signatureRawData: " + signatureRawData);
+        //Criando assinatura criptografada em hmacSha256
+        String requestSignatureBase64String = computeSignature(signatureRawData, APIkey);  
+        
+        //Criando Header AMX, a partir dos parâmetros gerados  
+        return String.format("amx %s:%s:%s:%s", APIid, requestSignatureBase64String, nonce, time); //Formato Header 
     }
 
-    public String request(String header, String function, String method, String data) throws Exception{
-        
+    public String request(String APIid, String APIkey, String function, String method, String data) throws Exception {
+       
+        //Criando parâmetros para conexão com a NegocieCoins
         URL url = new URL("https://broker.negociecoins.com.br/tradeapi/v1/" + function);
-        HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
-        //======================================================================       
+        HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();   
         urlConnection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
         urlConnection.setReadTimeout(15000);
         urlConnection.setConnectTimeout(15000);
-        urlConnection.setRequestMethod(method); 
-        urlConnection.setDoInput(true);
+        urlConnection.setRequestMethod(method);
         urlConnection.setDoOutput(true);
-        urlConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-        urlConnection.setRequestProperty("Content-Length", Integer.toString(data.getBytes().length));
-        //urlConnection.setRequestProperty("Content", data);
-        urlConnection.setRequestProperty("Authorization", header);
-        //urlConnection.connect();
-        //======================================================================
+        urlConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8"); 
+        
+        //Setando condições e atribuições para cada tipo de requisição
+        if ("POST".equals(method.toUpperCase())) {
+            urlConnection.setRequestProperty("Content-Length", Integer.toString(data.length()));
+            //urlConnection.setRequestProperty("Content", data);
+            String header = amx_authorization_header(url.toString(),APIid, APIkey, function, method, data);
+            urlConnection.setRequestProperty("Authorization", header);
+            //Tentar passar Conteudo via POST
+            try (OutputStream os = urlConnection.getOutputStream()) {
+                os.write(data.getBytes("UTF-8"));
+            }            
+            
+        } else {
+            //GET e DELETE não precisa de Content-Length
+            String header = amx_authorization_header(url.toString(),APIid, APIkey, function, method, data);
+            urlConnection.setRequestProperty("Authorization", header);
+        }
+        
         BufferedReader reader = null;
-        try{
-            //===========SEND REQUEST
-            if("POST".equals(method.toUpperCase())){
-            DataOutputStream wr = new DataOutputStream(urlConnection.getOutputStream());
-            System.out.println("URLEncoder to POST: " + URLEncoder.encode(data,"UTF-8"));
-            wr.writeBytes(URLEncoder.encode(data,"UTF-8"));
-            wr.flush();
-            wr.close();
-            }
-            //===========GET RESPONSE
+        try {  
+            //Receber resposta da requisição
             reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-            StringBuffer buffer = new StringBuffer();
+            StringBuilder buffer = new StringBuilder();
             int read;
             char[] chars = new char[1024];
             while ((read = reader.read(chars)) != -1) {
                 buffer.append(chars, 0, read);
             }
             return buffer.toString();
-            //===========
-            
+
         } finally {
             if (reader != null) {
                 reader.close();
             }
-        }       
-        //======================================================================
-        
+        }
+
     }
 
 }
--- Python/main.py
+++ Python/main.py
@@ -1,38 +1,49 @@
-import base64, hashlib, hmac, random, time, urllib, _md5
-from urllib.request import urlopen, Request
+import base64, hashlib, hmac, random, time, urllib, json, urllib2, requests
 
-def amx_authorization_header(id, key, url, method, body=None):
-   encoded_url = urllib.parse.quote_plus(url).lower()
+
+def amx_authorization_header(id, key, url, method, body):
+   print('Metodo: ' + method)
+   encoded_url = urllib.quote_plus(url).lower()
    method = method.upper()
    ##Trabalhando content///////////////////////////////////////////////////////
-   content = '' if body == None else base64.b64encode(_md5.new(body).digest())
+   m = hashlib.md5()
+   m.update(str(body))
+   content = '' if body == None else base64.b64encode(m.digest())
+   print('content: ' + content)
    ##==================////////////////////////////////////////////////////////
    timestamp = str(int(time.time()))
    nonce = str(random.randint(0, 100000000))
    data = ''.join([id, method, encoded_url, timestamp, nonce, content]).encode()
+   print('DATA: ' + data)
    secret = base64.b64decode(key)
-   signature = str(base64.b64encode(hmac.new(secret, msg=data, digestmod=hashlib.sha256).digest()), 'utf-8')
+   signature = str(base64.b64encode(hmac.new(secret, msg=data, digestmod=hashlib.sha256).digest()))
    header = 'amx %s' % ':'.join([id, signature, nonce, timestamp])
    print('HEADER: ' + header)
    return header             
-##base64.b64encode(hashlib.md5.new(body).digest())
 
-def exchange(url, result):
-    req = Request(url, headers={'Authorization': result, 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0'})
-    webpage = urllib.request.urlopen(req).read()
-    return webpage
+
+def exchange(url, result, metodo, jsondata):
+    
+    if(metodo=='GET'):
+       webpage = requests.get(url, headers={'Authorization': result, 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0'})
+       
+    elif(metodo=='POST'):
+       webpage = requests.post(url, data=jsondata, headers={'Authorization': result, 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0', 'Content-Type': 'application/json'})
+
+    return webpage.json()
 
 id1 = 'youID'
-key = 'youkey'
+key = 'youKey'
 url1 = 'https://broker.negociecoins.com.br/tradeapi/v1/'
-method = 'GET'
-function = 'user/balance'
+method = 'POST'
+function = 'user/orders'
 urlFinal = ''.join([url1, function])
-##body = "{'pair':'BRLBTC','type':'sell','status':'filled','startId':1,'endId':9999999,'startDate':'2017-01-01','endDate':'2017-12-31'}"
-result2 = amx_authorization_header(id1, key, urlFinal, method, body=None)
+jsonstr = '''{"pair" : "BRLBTC","type":"sell","status":"filled","startId": 1, "endId": 9999999,"startDate":"2017-01-01","endDate":"2017-12-31"}'''
+##jsonstr = ''
+result2 = amx_authorization_header(id1, key, urlFinal, method, jsonstr)
 
 print(result2)
 
-print(exchange(str(urlFinal), result2))
+print(exchange(str(urlFinal), result2, method, jsonstr))
 
 
